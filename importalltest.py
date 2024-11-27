69ba949384f17f52b1604129736e4b2621e40c8c
whylou <82345942+whylou@users.noreply.github.com>
2024-11-27 18:58:19 +0800

    Add files via upload

--- importalltest.py
+++ importalltest.py
@@ -1,8 +1,4 @@
 import mset
-Mat.setSubroutine('Emmision','Emmisive')
-tex = mset.Texture(Path)
-if keyword2!='Normal':
-    tex.sRGB=True
-    tex.useMipmaps=True
-if shader:
-    shader.setField("Emmisive Map", tex)
+sceneMaterials = mset.getAllMaterials()
+print(sceneMaterials[1].name)
+print(sceneMaterials[1].setSubroutine('Emissive', 'Emissive'))
